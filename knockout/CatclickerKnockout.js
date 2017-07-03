3757da101013849c442ba49330fcf074964e052c
ente78 <britta.ente@gmail.com>
2017-07-03 19:26:54 +0200

    dsFASDFFDSA

--- knockout/CatclickerKnockout.js
+++ knockout/CatclickerKnockout.js
@@ -2,16 +2,27 @@ var ViewModel = function(){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("ente");
 	this.imgSrc= ko.observable("ente.jpg");
+	
 
 	this.incrementCounter = function(){
 		this.clickCount(this.clickCount()+1); 
 
+	}
 
-	};
+	
+	this.levels = ko.computed(function(){
+		var levels;
+		if (clickCount < 10){
+			levels = "infant";			
+		} else {
+			levels = "master";			
+		}
+		return levels;
+		}, this);
+	}; 
 
-}
 
-
+return levels
 ko.applyBindings(new ViewModel()); 
 
 

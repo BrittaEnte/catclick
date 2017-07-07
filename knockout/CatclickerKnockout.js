029a8e274ab158b8f2dbcb5a4250c42242baaa5f
ente78 <britta.ente@gmail.com>
2017-07-07 19:43:41 +0200

    hh

--- knockout/CatclickerKnockout.js
+++ knockout/CatclickerKnockout.js
@@ -1,43 +1,42 @@
 
 // here we store the dates of the different ducks
-var duckmodel = function(){
-
-	ducks: [
+var initialduck = [
+	
 		{
 		count: 0,
-		name: 1,
+		name: "tabby",
 		img: "ente.jpg",
 		nickname: ["bla", "blubb"]
 		},
 		{
 		count: 0,
-		name: 2,
+		name: "fluffy",
 		img: "ente1.png",
-		nickname: ["bla", "blubb"]
+		nickname: ["wuff", "blubb"]
 		},
 		{
 		count: 0,
-		name: 3,
+		name: "blubby",
 		img: "ente2.jpg",
-		nickname: ["bla", "blubb"]
+		nickname: ["diff", "blubb"]
 		},
 		{
 		count: 0,
-		name: 4,
+		name: "duffy",
 		img: "ente3.jpg",
-		nickname: ["bla", "blubb"]
+		nickname: ["miau", "blubb"]
 		}
 	]	
-};
+
 
 
 
 // here we connect the data with the view. we choose knockout js to bind all datas
-var Duck = function(){
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable("ente");
-	this.imgSrc= ko.observable("ente.jpg");
-	this.nickname= ko.observableArray(["bla", "blubb"]);
+var Duck = function(data){
+	this.clickCount = ko.observable(data.count);
+	this.name = ko.observable(data.name);
+	this.imgSrc= ko.observable(data.img);
+	this.nickname= ko.observableArray(data.nickname);
 	
 
 	
@@ -60,15 +59,32 @@ var Duck = function(){
 
 // here we choose the duck clicked on 
 var ViewModel = function(){
+
 	var self = this;
-	this.currentDuck = ko.observable(new Duck());
+
+	this.ducklist = ko.observableArray([]);
+
+	initialduck.forEach(function(duckItem) {
+		// body...
+		self.ducklist.push(new Duck(duckItem));
+	});
+	
+
+	this.currentDuck = ko.observable(this.ducklist()[0]);
+
+	
 
 	// increment counter 
 	this.incrementCounter = function(){
-		self.currentDuck().clickCount(self.currentDuck().clickCount()+1); 
+		this.clickCount(this.clickCount()+1); 
 
-}
-}
+	};
+
+	this.setDuck = function(clickedDuck){
+		self.currentDuck(clickedDuck);
+
+	}; 
+};
 
 
 ko.applyBindings(new ViewModel()); 

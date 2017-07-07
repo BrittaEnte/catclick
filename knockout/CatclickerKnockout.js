660054680e0a752d4c6fdd5855fd01fe9f409285
ente78 <britta.ente@gmail.com>
2017-07-07 17:53:46 +0200

    tt

--- knockout/CatclickerKnockout.js
+++ knockout/CatclickerKnockout.js
@@ -1,15 +1,44 @@
-var ViewModel = function(){
+
+// here we store the dates of the different ducks
+var duckmodel = function(){
+
+	ducks: [
+		{
+		count: 0,
+		name: 1,
+		img: "ente.jpg",
+		nickname: ["bla", "blubb"]
+		},
+		{
+		count: 0,
+		name: 2,
+		img: "ente1.png",
+		nickname: ["bla", "blubb"]
+		},
+		{
+		count: 0,
+		name: 3,
+		img: "ente2.jpg",
+		nickname: ["bla", "blubb"]
+		},
+		{
+		count: 0,
+		name: 4,
+		img: "ente3.jpg",
+		nickname: ["bla", "blubb"]
+		}
+	]	
+};
+
+
+
+// here we connect the data with the view. we choose knockout js to bind all datas
+var Duck = function(){
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable("ente");
 	this.imgSrc= ko.observable("ente.jpg");
 	this.nickname= ko.observableArray(["bla", "blubb"]);
-
 	
-
-	this.incrementCounter = function(){
-		this.clickCount(this.clickCount()+1); 
-
-	}
 
 	
 	this.levels = ko.computed(function(){
@@ -25,7 +54,21 @@ var ViewModel = function(){
 
 		return levels;
 		}, this);
-	}; 
+	
+}
+
+
+// here we choose the duck clicked on 
+var ViewModel = function(){
+	var self = this;
+	this.currentDuck = ko.observable(new Duck());
+
+	// increment counter 
+	this.incrementCounter = function(){
+		self.currentDuck().clickCount(self.currentDuck().clickCount()+1); 
+
+}
+}
 
 
 ko.applyBindings(new ViewModel()); 
